f1b539a273072feb16e7daab86df1ea518b2f8f7
Franco Dreer <francodreer@gmail.com>
2023-09-01 18:18:44 -0300

    probando nuevo deploy

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,7 +4,7 @@ import {
   corsConfig,
   httpTimeOut,
   limitPayloadSize,
-  requestLimiter,
+  requestLimiter
 } from '../security/securityServer'
 import connectionDB from '../config/mongo'
 import rootRouter from '../routes'
@@ -16,16 +16,14 @@ connectionDB()
 
 app.disable('x-powered-by')
 app.use(corsConfig)
-app.use(requestLimiter)
-app.use(httpTimeOut)
+// app.use(requestLimiter)
+// app.use(httpTimeOut)
 app.use(express.urlencoded({ extended: true, limit: '50mb' })) // ❓❓❓
 app.use(express.json())
-app.use(limitPayloadSize)
+// app.use(limitPayloadSize)
 
 // Se utiliza el index de todas las rutas
 app.use('/api', rootRouter)
-
-app.use(express.static('public'))
 
 app.get('/', (_req, res) => {
   return res.send('Hello World!')
